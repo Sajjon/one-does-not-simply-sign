b87306c562f42a1903b935c68b7acb0b2b1454e9
Alexander Cyon <alex.cyon@gmail.com>
2024-08-09 13:31:37 +0200

    remove code, preparing for write using generics.

--- src/owned_types/mod.rs
+++
@@ -1,5 +0,0 @@
-mod owned;
-mod owned_factor_instance;
-
-pub use owned::*;
-pub use owned_factor_instance::*;
--- src/signatures_outcome_types/mod.rs
+++
@@ -1,5 +0,0 @@
-mod maybe_signed_transactions;
-mod signatures_outcome;
-
-pub use maybe_signed_transactions::*;
-pub use signatures_outcome::*;
