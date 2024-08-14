5bd7bbf7b15d6ef544cb259f88a5a1e63b87d0cd
Alexander Cyon <alex.cyon@gmail.com>
2024-08-14 11:23:47 +0200

    Rename to SignaturesCollector

--- src/drivers/use_factor_source_client.rs
+++ src/drivers/use_factor_source_client.rs
@@ -12,37 +12,37 @@ impl UseFactorSourceClient {
     pub async fn use_factor_sources(
         &self,
         factor_sources: IndexSet<FactorSource>,
-        coordinator: &FactorResultsBuildingCoordinator,
+        collector: &SignaturesCollector,
     ) -> Result<()> {
         match &self.driver {
             // Parallel Driver: Many Factor Sources at once
             UseFactorSourceDriver::ParallelBatch(driver) => {
                 // Prepare the request for the driver
-                let request = coordinator.request_for_parallel_batch_driver(
+                let request = collector.request_for_parallel_batch_driver(
                     factor_sources.into_iter().map(|f| f.id).collect(),
                 );
                 let response = driver.sign(request).await?;
-                coordinator.process_batch_response(response);
+                collector.process_batch_response(response);
 
                 Ok(())
             }
 
             // Serial Driver: One Factor Sources at a time
-            // After each factor source we pass the result to the coordinator
+            // After each factor source we pass the result to the collector
             // updating its internal state so that we state about being able
             // to skip the next factor source or not.
             UseFactorSourceDriver::SerialBatch(driver) => {
                 for factor_source in factor_sources {
                     // Prepare the request for the driver
-                    let request = coordinator.request_for_serial_batch_driver(&factor_source.id);
+                    let request = collector.request_for_serial_batch_driver(&factor_source.id);
 
                     // Produce the results from the driver
                     let response = driver.sign(request).await?;
 
-                    // Report the results back to the coordinator
-                    coordinator.process_batch_response(response);
+                    // Report the results back to the collector
+                    collector.process_batch_response(response);
 
-                    if !coordinator.continue_if_necessary()? {
+                    if !collector.continue_if_necessary()? {
                         break;
                     }
                 }
@@ -54,7 +54,7 @@ impl UseFactorSourceClient {
 
                 'loop_0: for factor_source in factor_sources.clone() {
                     let requests_per_transaction =
-                        coordinator.requests_for_serial_single_driver(&factor_source.id);
+                        collector.requests_for_serial_single_driver(&factor_source.id);
 
                     let mut outputs = IndexSet::<HDSignature>::new();
 
@@ -79,7 +79,7 @@ impl UseFactorSourceClient {
 
                     per_factor_source_outputs.insert(factor_source.id, outputs);
 
-                    if !coordinator.continue_if_necessary()? {
+                    if !collector.continue_if_necessary()? {
                         break 'loop_0;
                     }
                 }
@@ -103,11 +103,11 @@ impl UseFactorSourceClient {
                 let ids_of_skipped_factors_sources =
                     all_ids.difference(&done_ids).cloned().collect();
 
-                // Report the results back to the coordinator, as a batch response
-                coordinator.process_batch_response(SignWithFactorSourceOrSourcesOutcome::Signed {
+                // Report the results back to the collector, as a batch response
+                collector.process_batch_response(SignWithFactorSourceOrSourcesOutcome::Signed {
                     produced_signatures: BatchSigningResponse::new(per_factor_source_outputs),
                 });
-                coordinator.process_batch_response(SignWithFactorSourceOrSourcesOutcome::Skipped {
+                collector.process_batch_response(SignWithFactorSourceOrSourcesOutcome::Skipped {
                     ids_of_skipped_factors_sources,
                 });
 
